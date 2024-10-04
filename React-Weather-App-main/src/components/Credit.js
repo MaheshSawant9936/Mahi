c4fd071810ccbb8d75d706560dedeb5b05fadfd7
React-whether-app <maheshsawant9936@gmail.com>
2024-10-04 10:48:59 +0530

    f

---
+++ React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+        Coded by
+      <a href="https://github.com/MaheshSawant9936"
+        > MaheshSawant9936</a
+      > 
+    </p>
+  </div>
+  );
+}
